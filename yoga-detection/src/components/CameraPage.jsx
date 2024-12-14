5fa19def19a5fc9f6dd68e7841ebc3b3277ad9d0
Yeshu <playeryeshu@gmail.com>
2024-12-14 18:20:41 +0530

    cameraUpdated

--- yoga-detection/src/components/CameraPage.jsx
+++ yoga-detection/src/components/CameraPage.jsx
@@ -1,11 +1,12 @@
 const CameraPage = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center' }} className="flex flex-col items-center justify-center h-screen my-20">
       <h1>Yoga Pose Detection</h1>
       <img
         src="http://127.0.0.1:5000/video_feed"
         alt="Yoga Pose Detection Stream"
         style={{ width: '80%', height: 'auto', border: '2px solid black' }}
+        className=""
       />
     </div>
   );
